194297619ee7d8781e39f1f4ee11a6abbc66aaa5
Camille Tromp <camille.tromp@gmail.com>
2023-09-27 11:25:12 +0200

    ajout section oscar

---
+++ wp-content/themes/foce-child/template-parts/oscar.php
@@ -0,0 +1,9 @@
+<!--  <section class="oscar"> 
+  <h4>Fleurs d'oranger & chats errants est nominé aux Oscars Short Film animated de 2022 ! </h4>
+  <img src="./assets/images/Logo_oscars.png" alt="logo oscars">
+</section>
+-->
+
+<section class="oscar"> 
+    <img src="wp-content/themes/foce-child/assets/images/Section_oscars.png" alt="logo oscars">
+</section>

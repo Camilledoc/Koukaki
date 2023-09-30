589ba66b3696bd7888dca41f232fe27ff44f1278
Camille Tromp <camille.tromp@gmail.com>
2023-09-30 15:59:31 +0200

    animation titres de section

--- wp-content/themes/foce-child/assets/javascript/script.js
+++ wp-content/themes/foce-child/assets/javascript/script.js
@@ -1,3 +1,4 @@
+//animation fleurs en rotation au scroll 
 const root=document.querySelector(':root');
 
 
@@ -8,3 +9,30 @@ root.style.setProperty('--speed', '2s');
 document.addEventListener('scrollend', function(){
     root.style.setProperty('--speed', '10s');
     })
+
+//animaiton titres section 
+// Create the observer
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('anim_title');
+      }
+    });
+  });
+  
+  observer.observe(document.querySelector('.section_title'));
+  observer.observe(document.querySelector('.section_title1'));
+
+  const observer2 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('anim_title2');
+      }
+    });
+  });
+  observer2.observe(document.querySelector('.section_title2'));
+
+  

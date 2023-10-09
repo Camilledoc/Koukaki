c8807b498f386da4eba5469ad7f95e594b17c441
Camille Tromp <camille.tromp@gmail.com>
2023-10-09 22:49:18 +0200

    menu toggle animé

--- wp-content/themes/foce-child/assets/javascript/script.js
+++ wp-content/themes/foce-child/assets/javascript/script.js
@@ -104,9 +104,42 @@ document.addEventListener('scroll', () => {
 )
 
   //menu toggle ou hamburger 
-const menuLine = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu');
+  const theToggle = document.querySelector('.toggle');
 
-menuLine.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
+// hasClass
+function hasClass(elem, className) {
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+// addClass
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+    	elem.className += ' ' + className;
+    }
+}
+// removeClass
+function removeClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+	if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+// toggleClass
+function toggleClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+theToggle.onclick = function() {
+   toggleClass(this, 'on');
+   return false;
+}
--- wp-content/themes/foce-child/header.php
+++ wp-content/themes/foce-child/header.php
@@ -26,19 +26,33 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'foce' ); ?></a>
 
 	<header id="masthead" class="site-header">
+
+    
 		<nav id="site-navigation" class="main-navigation">
-            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-                <span class="line"></span>
-                <span class="line"></span>
-                <span class="line"></span>
+        <div class="menu">
+                <a class="site-title"href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+            </div>
+            <button class="toggle">
+            <a href="#fullscreenmenu"></a>
+            <span></span>
             </button>
-            <ul class="menu">
+            <ul id="fullscreenmenu">
+            <img class="logo-mini" src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
                 <li><a href="#story">Histoire</a></li>
                 <li><a href="#characters">Personnages</a></li>
-                <li class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
                 <li><a href="#place">Lieu</a></li>
                 <li><a href="#studio">Studio Koukaki</a></li>
+                <p> STUDIO KOUKAKI</p>
+                <img class="cat" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/menu/cat.png'; ?> " alt="tête de chat animée violet">
+                <img class="cat-1"src="<?php echo get_stylesheet_directory_uri() . '/assets/images/menu/cat-1.png'; ?> " alt="tête de chat animée orange">
+                <img class="cat-2"src="<?php echo get_stylesheet_directory_uri() . '/assets/images/menu/cat-2.png'; ?> " alt="tête de chat animée noir">
+                <img class="flower" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/menu/Flower.png'; ?> " alt="fleur points orange">
+                <img class="flower-1" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/menu/hibiscus_footer.png'; ?> " alt="hibiscus orange">
+                <img class="flower-2" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/menu/orchid.png'; ?> " alt="orchidée violette">
+                <img class="flower-3" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/menu/random_flower.png'; ?> " alt="fleur rose">
+                <img class="flower-4" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/menu/Sunflower.png'; ?> " alt="Fleur blanche">
             </ul>
-
+           
+            
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
